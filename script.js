0fdd474f7761b2c21a19cc472e393a789e3ad748
Aditya Shankar <64427315+shankar-aditya@users.noreply.github.com>
2020-12-18 17:56:18 +0530

    Added retention of theme

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('light')
+}
+else{
+    setTheme(theme)
+}
+
 let themeDots = document.getElementsByClassName('theme-dot')
 
 for(var i=0; themeDots.length>i; i++){
@@ -21,4 +30,6 @@ function setTheme(mode){
     if(mode == 'purple'){
         document.getElementById('theme-style').href = 'purple.css'
     }
+
+    localStorage.setItem('theme',mode)
 }
